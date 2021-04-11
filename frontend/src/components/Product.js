103dc53cf83500d81c638ed9d001792913ba4b29
Kedar Gupta <kedargupta@msn.com>
2021-04-11 18:31:53 +1200

    Fix a links in Product

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,20 +1,26 @@
-import {Card} from 'react-bootstrap'
+import { Card } from "react-bootstrap";
 
-const Product = ( { product } ) => {                               // Using destructuring to access props.product 
-    return (
-        <Card className='my-3 p-3 rounded'>
-            <a href={`/products/${product._id}`}>
-                <Card.Img src={product.image} variant='top' />
-                <Card.Body>
-                    <Card.Title as='div'><strong>{product.name}</strong></Card.Title>
-                    <Card.Text as='div'>
-                        {product.rating} from {product.numReviews} reviews
-                    </Card.Text>
-                    <Card.Text as='h3'>${product.price}</Card.Text>
-                </Card.Body>
-            </a>
-        </Card>
-    )
-}
+const Product = ({ product }) => {
+  // Using destructuring to access props.product
+  return (
+    <Card className="my-3 p-3 rounded">
+      <a href={`/products/${product._id}`}>
+        <Card.Img src={product.image} variant="top" />
+      </a>
 
-export default Product
+      <Card.Body>
+        <a href={`/products/${product._id}`}>
+          <Card.Title as="div">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </a>
+        <Card.Text as="div">
+          {product.rating} from {product.numReviews} reviews
+        </Card.Text>
+        <Card.Text as="h3">${product.price}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Product;
